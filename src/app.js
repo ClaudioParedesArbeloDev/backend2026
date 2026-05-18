813f360c998dc34cea9a5dee52cf1403c2fa78a9
Claudio Paredes <claudioparedesarbelo@gmail.com>
2026-05-18 18:41:54 -0300

    express avanzado

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
 //los verbos HTTP son: GET, POST, PUT, DELETE, PATCH
 /* Que es un CRUD?
 C: Create -> POST
@@ -12,15 +16,17 @@ D: Delete -> DELETE
 
 //Array de objetos que va a actuar de base de datos
 const usuarios = [
-    {id: 1, nombre: 'Claudio', apellido: 'Gomez', edad: 30},
-    {id: 2, nombre: 'Maria', apellido: 'Perez', edad: 25},
-    {id: 3, nombre: 'Juan', apellido: 'Lopez', edad: 35},
-    {id: 4, nombre: 'Ana', apellido: 'Garcia', edad: 28},
+    {id: 1, nombre: 'Claudio', apellido: 'Gomez', edad: 30, genero: 'M'},
+    {id: 2, nombre: 'Maria', apellido: 'Perez', edad: 25, genero: 'F'},
+    {id: 3, nombre: 'Juan', apellido: 'Lopez', edad: 35, genero: 'M'},
+    {id: 4, nombre: 'Ana', apellido: 'Garcia', edad: 28, genero: 'F'},
 ]
 
-let idA = 1
-let user = usuarios.find(u => u.id === idA)
-console.log(user)
+let users = []
+
+// let idA = 1
+// let user = usuarios.find(u => u.id === idA)
+// console.log(user)
 
 
 app.get('/saludo', (req, res)=>{
@@ -54,6 +60,67 @@ app.get('/usuarios/:idUsuario', (req, res)=>{
     res.send({usuario})
 })
 
+app.get('/ejemploQueries', (req, res)=>{
+    let consulta = req.query;
+
+    let {nombre, apellido, edad} = req.query;
+
+    res.send(consulta);
+})
+
+app.get('/', (req, res)=>{
+    
+    let genero = req.query.genero;
+
+    if(!genero || (genero !== 'M' && genero !== 'F')) return res.send({usuarios})
+
+    let usuariosFiltrado = usuarios.filter( usuario => usuario.genero === genero);
+    res.send({usuarios: usuariosFiltrado})
+})
+
+app.get('/api/user', (req, res)=>{
+    res.send(users)
+})
+
+app.post('/api/user', (req, res)=>{
+
+    let user = req.body;
+
+    if(!user.first_name || !user.last_name){
+        return res.status(400).send({status:'error', error:'Valores incompletos'})
+    }
+
+    users.push(user);
+    res.send({status:'success', message: 'Usuario Creado'})
+})
+
+app.put('/api/user/:name', (req, res)=>{
+    let {name} = req.params
+    let update = req.body;
+
+    const index = users.findIndex(u => u.first_name === name);
+
+    if (index === -1){
+        return res.status(404).json({message: `User ${name} not found`});
+    }
+
+    users[index] = {...users[index], ...update};
+
+    res.status(200).json(users[index])
+
+})
+
+app.delete('/api/user/:name', (req, res) => {
+    let {name} = req.params
+    let currentLength = users.length;
+
+    users = users.filter(user => user.first_name !== name);
+    if(users.length === currentLength){
+        return res.status(404).send({status: "error", error: "User not found"})
+    }
+
+    res.send({status:"success", message: "User deleted"})
+})
 
 
 app.listen(8080, () =>console.log('Servidor escuchando en el puerto 8080'))
